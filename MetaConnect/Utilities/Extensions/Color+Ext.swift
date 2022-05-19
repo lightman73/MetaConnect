4cd1f513bbd72497fb3df88447d58c6c94e6e1ce
Francesco Marini <franz.marini@gmail.com>
2022-05-19 11:38:15 +0200

    feat: add brandSecondary color and optimise brandPrimary

--- MetaConnect/Utilities/Extensions/Color+Ext.swift
+++ MetaConnect/Utilities/Extensions/Color+Ext.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 extension Color {
     static let brandPrimary = Color("brandPrimary")
+    static let brandSecondary = Color("brandSecondary")
     static let metaMask = Color("metaMask")
 }
 
 extension UIColor {
     static let brandPrimary = UIColor(named: "brandPrimary")
+    static let brandSecondary = UIColor(named: "brandSecondary")
     static let metaMask = UIColor(named: "metaMask")
 }
